57ae54a7b3d71c6308d28634b601891a53ec6dd6
KadirDuran <kadirduran61@gmail.com>
2024-08-04 20:38:54 +0300

    Geliştirmeler yapıldı.
    Ürün detay sayfası eklendi.
    Viewler API ile birleştirildi.

--- app/src/main/java/com/example/storeapp/OneProduct.kt
+++ app/src/main/java/com/example/storeapp/OneProduct.kt
@@ -10,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.storeapp.databinding.ActivityMainBinding
 import com.example.storeapp.databinding.ActivityOneProductBinding
+import com.squareup.picasso.Picasso
 import java.io.Serializable
 import java.util.function.LongBinaryOperator
 import kotlin.time.Duration
@@ -33,8 +34,18 @@ class OneProduct : AppCompatActivity()   {
             insets
         }
         val intents = intent
-        val product = intents.getSerializableExtra("selectedItem") as Product
-         Toast.makeText(this,"Gelen veri degeri " + product.title,Toast.LENGTH_LONG).show()
+        val product: Product = intents.getSerializableExtra("selectedItem") as Product
+
+        Picasso.get().load(product.imgUrl).into(binding.productImg)
+        binding.txtProductPrice.text ="Price\n"+ product.price.toString() +  " $"
+        binding.txtProductName.text = product.title.uppercase()
+        binding.txtProductDesc.text ="Description\n"+ product.description
+        binding.txtCat.text ="Category : " + product.category.uppercase()
+        binding.txtRating.text =product.rate.toString() +" / "+product.count.toString();
+
+        /*Taslaklar oluşturup eşleştirmesi yapılacak!
+
+         */
     }
 }
 
